3ed91d9a08aec039bbf948ee40b428e8f555ff5a
wispray74 <wispray74@gmail.com>
2026-03-05 11:25:14 +0700

    Update crypto.js

--- server/services/crypto.js
+++ server/services/crypto.js
@@ -2,6 +2,6 @@ const CryptoJS = require('crypto-js');
 const KEY = process.env.ENCRYPT_KEY || 'default_key_change_in_production!!';
 
 function encrypt(text) { return CryptoJS.AES.encrypt(text, KEY).toString(); }
-function decrypt(cipher) { return CryptoJS.AES.decrypt(cipher, KEY).bytes.toString(CryptoJS.enc.Utf8); }
+function decrypt(cipher) { return CryptoJS.AES.decrypt(cipher, KEY).toString(CryptoJS.enc.Utf8); }
 
 module.exports = { encrypt, decrypt };
